06d8e3b425a6d4f6ce3eb04caac4e2158caa278c
vipulagr09 <vipulagr@hotmail.com>
2021-08-18 14:41:00 +0530

    adding new logger
    added few more logger

---
+++ src/main/java/com/example/elk/ProjectForElkApplication.java
@@ -0,0 +1,13 @@
+package com.example.elk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectForElkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectForElkApplication.class, args);
+	}
+
+}
